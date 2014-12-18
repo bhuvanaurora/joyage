0a95a4ad1e4e4e5addadacba341d5be13d0486ad
bhuvanaurora <bhuvan.aurora@gmail.com>
2014-12-18 18:11:09 +0530

    navbar drop down improved

--- public/controllers/detail.js
+++ public/controllers/detail.js
@@ -432,4 +432,76 @@ angular.module('MyApp')
       }());
 
       });
+
+     // ----------------------------------------------- For Navbar --------------------------------------------- //
+
+     $(".js-navigation").click (function(){
+         $(this).toggleClass("js-open-nav");
+         $("nav").toggleClass("js-open-nav");
+     });
+
+     $(".js-filter-activity:not(.js-activity-holder)").click(function() {
+         $('.js-activity-holder').slideToggle(200);
+     });
+
+     $('.js-filter-place:not(.js-place-holder)').click(function() {
+         $('.js-place-holder').slideToggle(200);
+     });
+
+     $(document).ready(function() {
+
+         $('.js-city-select').click(function() {
+             $('.js-city-select-drawer').fadeToggle('fast', function() {
+                 // Animation complete
+             });
+             return false;
+         });
+
+         // Even when the window is resized, run this code.
+         $(window).resize(function(){
+
+             // Variables
+             var windowHeight = $(window).height();
+
+             // Find the value of 90% of the viewport height
+             var ninetypercent = .085 * windowHeight;
+
+             // When the document is scrolled ninety percent, toggle the classes
+             // Does not work in iOS 7 or below
+             // Hasn't been tested in iOS 8
+             $(document).scroll(function(){
+
+                 // Store the document scroll function in a variable
+                 var y = $(this).scrollTop();
+
+                 // If the document is scrolled 90%
+                 if( y > ninetypercent) {
+                     $('.js-main-header').addClass('js-sticky-header');
+                 } else {
+                     $('.js-main-header').removeClass('js-sticky-header');
+                 }
+             });
+
+             // Call it on resize.
+         }).resize();
+
+     });
+
+     // -------------- for modal
+
+     $('.open-modal').click(function() {
+         var el = $(this)[0],
+             classes = el.className.split(/\s+/);
+         for(var i = 0; i < classes.length; i++) {
+             if(classes[i].match(/modal-/)) {
+                 var modalClass = classes[i];
+                 $('.modal.' + modalClass).fadeIn('fast');
+             }
+         }
+     });
+
+     $('.close-modal').click(function() {
+         $(this).closest('.modal').fadeOut('fast');
+     });
+
     }]);
