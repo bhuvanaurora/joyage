b6c0395849173a3fcd62a46d0abd836e6bf3eaf9
bhuvanaurora <bhuvan.aurora@gmail.com>
2014-12-19 11:36:33 +0530

    Invite button working on locahost

--- public/controllers/inviteCtrl.js
+++ public/controllers/inviteCtrl.js
@@ -1,75 +1,54 @@
 angular.module('MyApp')
-  .controller('InviteCtrl', ['$scope', '$window', '$alert', '$window', '$routeParams', '$http', '$route', '$templateCache', 'fb_appId', 'Profile', 'editedProfile', 'Invites', 'updateInvites',
-    function($scope, $window, $alert, $window, $routeParams, $http, $route, $templateCache, fb_appId, Profile, editedProfile, Invites, updateInvites) {
-
-      var randomString;
+  .controller('InviteCtrl', ['$scope', '$window', '$alert', '$window', '$routeParams', '$http', '$route', 'fb_appId', 'fb_connect', 'Profile', 'editedProfile', 'Invites', 'updateInvites',
+    function($scope, $window, $alert, $window, $routeParams, $http, $route, fb_appId, fb_connect, Profile, editedProfile, Invites, updateInvites) {
 
       editedProfile.get({ _id: $routeParams.id }, function(profile) {
         $scope.profile = profile;
 
         //$scope.disabled = true;
-        if ($scope.profile.inviteSent) {
+        /*if ($scope.profile.inviteSent) {
           if ($scope.profile.inviteSent < 10) {
             $scope.disabled = false;
           }
+        }*/
+
+        if (!$scope.profile.facebook.inviteString) {
+
+          chance.set('lastNames', ['Underwood', 'Stamper', 'Meechum', 'Vasquez', 'Gallagher', 'Danton', 'Posner', 'Barnes', 'Russo', 'Bing',
+            'Geller', 'Buffay', 'Tribbiani', 'Green', 'Arryn', 'Baratheon', 'Bolton', 'Frey', 'Greyjoy', 'Lannister', 'Martell', 'Stark',
+            'Targaryen', 'Tully', 'Tyrell']);
+
+          randomString = chance.last() + '-' + chance.last() + chance.cc() + chance.hashtag();
+          var newString = true;
+        } else {
+          randomString = $scope.profile.facebook.inviteString;
         }
 
-        $window.fbAsyncInit = function () {
+        $window.fbAsyncInit = function() {
           FB.init({
             appId: fb_appId,
             responseType: 'token',
             version: 'v2.2',
-            cookie: true,
-            status: true,
-            xfbml: true,
             oauth: true,
-            frictionlessRequests: true
+            xfbml: true
           });
+        };
 
-          (function () {
-            FB.api('/v2.2/me/friends?fields=name,location,id', function (response) {
-              if (response && !response.error) {
-                $scope.friend_list = response;
-              }
-            })
-          }());
-
-          /*(function() {
-           var e = document.createElement(script);
-           e.async = true;
-           e.src = document.location.protocol + '//connect/facebook.net/en_US/all.js';
-           document.getElementById('fb-root').appendChild(e);
-           }());*/
-
-          /*$scope.fb_invite = function (id) {
-           FB.ui({
-           to: id,
-           method: 'send',
-           name: 'Joyage | Discover the best activities in town',
-           link: 'http://joyage.in'
-           });
-           };
-
-           $scope.fb_invite = function () {
-           FB.ui({
-           to: '',
-           method: 'send',
-           name: 'Joyage | Discover the best activities in town',
-           link: 'http://joyage.in'
-           });
-           };*/
-
-          if (!$scope.profile.facebook.inviteString) {
-
-            chance.set('lastNames', ['Underwood', 'Stamper', 'Meechum', 'Vasquez', 'Gallagher', 'Danton', 'Posner', 'Barnes', 'Russo', 'Bing',
-              'Geller', 'Buffay', 'Tribbiani', 'Green', 'Arryn', 'Baratheon', 'Bolton', 'Frey', 'Greyjoy', 'Lannister', 'Martell', 'Stark',
-              'Targaryen', 'Tully', 'Tyrell']);
-
-            randomString = chance.last() + '-' + chance.last() + chance.cc() + chance.hashtag();
-            console.log(randomString);
-          } else {
-            randomString = $scope.profile.facebook.inviteString;
+        // Asynchronously load Facebook SDK
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) {
+            return;
           }
+          js = d.createElement(s);
+          js.id = id;
+          js.src = fb_connect;
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+          $scope.fb_invite = function () {
+            console.log("Im ready to invite");
+          };
 
           $scope.fb_request = function () {
             $alert({
@@ -96,7 +75,9 @@ angular.module('MyApp')
                   link: 'http://joyage.co/login/' + randomString
                 });
               }
-              Invites.save({ id: randomString });
+              if(newString) {
+                Invites.save({id: randomString});
+              }
               $scope.profile.$update(function () {
                 $route.reload();
               });
@@ -118,7 +99,39 @@ angular.module('MyApp')
               }
             });
           };
-        };
+
+          /*(function () {
+           FB.api('/v2.2/me/friends?fields=name,location,id', function (response) {
+           if (response && !response.error) {
+           $scope.friend_list = response;
+           }
+           })
+           }());*/
+
+          /*(function() {
+           var e = document.createElement(script);
+           e.async = true;
+           e.src = document.location.protocol + '//connect/facebook.net/en_US/all.js';
+           document.getElementById('fb-root').appendChild(e);
+           }());*/
+
+          /*$scope.fb_invite = function (id) {
+           FB.ui({
+           to: id,
+           method: 'send',
+           name: 'Joyage | Discover the best activities in town',
+           link: 'http://joyage.in'
+           });
+           };
+
+           $scope.fb_invite = function () {
+           FB.ui({
+           to: '',
+           method: 'send',
+           name: 'Joyage | Discover the best activities in town',
+           link: 'http://joyage.in'
+           });
+           };*/
 
       });
 
