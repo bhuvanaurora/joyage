83efede22d87f4794b1c01b6aee77ada79c58295
bhuvanaurora <bhuvan.aurora@gmail.com>
2014-12-29 15:03:56 +0530

    slider added

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -225,4 +225,26 @@ angular.module('MyApp')
         $(this).closest('.modal').fadeOut('fast');
       });
 
+      // -------------- for slider
+
+      var $firstSlide = $('.slide:first-child');
+      var $lastSlide = $('.slide:last-child');
+      $('.slide:not(".js-active")').hide();
+      $('.js-control-left').click(function() {
+        if ($('li.js-active').index() == 0) {
+          $('.js-active').fadeOut(500).removeClass('js-active');
+          $lastSlide.toggleClass('js-active').fadeIn(500);
+        } else {
+          $('.js-active').fadeOut(500).removeClass('js-active').prev('li').toggleClass('js-active').fadeIn(500);
+        }
+      });
+      $('.js-control-right').click(function() {
+        if ($('li.js-active').index() == 3) {
+          $('.js-active').fadeOut(500).removeClass('js-active');
+          $firstSlide.toggleClass('js-active').fadeIn(500);
+        } else {
+          $('.js-active').removeClass('js-active').fadeOut(500).next('li').toggleClass('js-active').fadeIn(500);
+        }
+      });
+
   }]);
