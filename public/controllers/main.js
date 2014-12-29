69fce6a453780241c612d818890ab15d414e151e
bhuvanaurora <bhuvan.aurora@gmail.com>
2014-12-29 15:53:12 +0530

    minor tweak

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -133,14 +133,16 @@ angular.module('MyApp')
     };
 
     $scope.pageClick = function(value) {
-      console.log($scope.headingTitle);
       if ($scope.headingTitle !== "all activities") {
         console.log($scope.activities);
         $scope.activities.push(Activity.query({ genre: $scope.headingTitle, page: value, sortOrder: $scope.sortOrder }));
         console.log(Activity.query({ genre: $scope.headingTitle, page: value, sortOrder: $scope.sortOrder }));
         console.log($scope.activities);
       } else {
+        console.log($scope.activities);
         $scope.activities.push(Activity.query({ page: value, sortOrder: $scope.sortOrder }));
+        console.log(Activity.query({ page: value, sortOrder: $scope.sortOrder }));
+        console.log($scope.activities);
       }
     };
     
