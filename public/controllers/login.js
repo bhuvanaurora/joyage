c94785ebc47beec5128ec2c57ce7192b93e59712
bhuvanaurora <bhuvan.aurora@gmail.com>
2015-03-30 18:40:23 +0530

    Landing page built

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -20,6 +20,76 @@ angular.module('MyApp')
     };
     $scope.pageClass = 'fadeZoom';
 
+      $('.landPageSec1').mouseover(function() {
+        var el1 = document.getElementById('landPage1');
+        var el2 = document.getElementById('landPage11');
+        el1.style.display = 'block';
+        el2.style.display = 'none';
+      });
+
+      $('.landPageSec1').mouseleave(function() {
+        var el1 = document.getElementById('landPage1');
+        var el2 = document.getElementById('landPage11');
+        el1.style.display = 'none';
+        el2.style.display = 'block';
+      });
+
+      $('.landPageSec2').mouseover(function() {
+        var el1 = document.getElementById('landPage2');
+        var el2 = document.getElementById('landPage22');
+        el1.style.display = 'block';
+        el2.style.display = 'none';
+      });
+
+      $('.landPageSec2').mouseleave(function() {
+        var el1 = document.getElementById('landPage2');
+        var el2 = document.getElementById('landPage22');
+        el1.style.display = 'none';
+        el2.style.display = 'block';
+      });
+
+      $('.landPageSec3').mouseover(function() {
+        var el1 = document.getElementById('landPage3');
+        var el2 = document.getElementById('landPage33');
+        el1.style.display = 'block';
+        el2.style.display = 'none';
+      });
+
+      $('.landPageSec3').mouseleave(function() {
+        var el1 = document.getElementById('landPage3');
+        var el2 = document.getElementById('landPage33');
+        el1.style.display = 'none';
+        el2.style.display = 'block';
+      });
+
+      $('.landPageSec4').mouseover(function() {
+        var el1 = document.getElementById('landPage4');
+        var el2 = document.getElementById('landPage44');
+        el1.style.display = 'block';
+        el2.style.display = 'none';
+      });
+
+      $('.landPageSec4').mouseleave(function() {
+        var el1 = document.getElementById('landPage4');
+        var el2 = document.getElementById('landPage44');
+        el1.style.display = 'none';
+        el2.style.display = 'block';
+      });
+
+      $('.landPageSec5').mouseover(function() {
+        var el1 = document.getElementById('landPage5');
+        var el2 = document.getElementById('landPage55');
+        el1.style.display = 'block';
+        el2.style.display = 'none';
+      });
+
+      $('.landPageSec5').mouseleave(function() {
+        var el1 = document.getElementById('landPage5');
+        var el2 = document.getElementById('landPage55');
+        el1.style.display = 'none';
+        el2.style.display = 'block';
+      });
+
       $('.open-modal').click(function() {
         $('.login-box-block').hide();
         var el = $(this)[0],
