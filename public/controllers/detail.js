3b789a3bfbae0c7de2030e11838181e572b28d96
bhuvanaurora <bhuvan.aurora@gmail.com>
2015-05-01 15:44:57 +0530

    $window.location wrongly user Test2

--- public/controllers/detail.js
+++ public/controllers/detail.js
@@ -4,7 +4,7 @@ angular.module('MyApp')
                              function($scope, $interval, $rootScope, $window, $routeParams, $location, $alert, $upload, fb_appId, fb_connect, Activity, Profile, Subscription, DoneIt, Tips, Selfies, Accept, Delete, Session, Auth) {
 
          if (!$rootScope.currentUser) {
-             $window.location('/');
+             $window.location = '/';
          }
 
          $window.scrollTo(0,0);                                 // To scroll to the top of the page
